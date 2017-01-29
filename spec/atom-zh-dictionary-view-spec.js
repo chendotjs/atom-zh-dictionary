c6d66cc376d470caa5a5bd4a174c42d1d148635d
chendotjs <chendotjs@qq.com>
2017-01-29 22:09:20 +0800

    initial commit

---
+++ spec/atom-zh-dictionary-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomZhDictionaryView from '../lib/atom-zh-dictionary-view';
+
+describe('AtomZhDictionaryView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
